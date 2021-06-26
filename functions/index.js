fda46ead68a52be91600c798aa40581cdb0e427c
Leonardo Matos <leomp120894@gmail.com>
2021-06-26 16:31:41 -0300

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -133,7 +133,7 @@ exports[functionName] = functions.https.onRequest(server)
 console.log(`-- Starting '${app.title}' E-Com Plus app with Function '${functionName}'`)
 
 // schedule update tokens job
-const cron = '35 * * * *'
+const cron = '38 * * * *'
 exports.updateTokens = functions.pubsub.schedule(cron).onRun(() => {
   return prepareAppSdk().then(appSdk => {
     return appSdk.updateTokens()
